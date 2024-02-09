5d51ababf81e28d60b409da17f9f491f479c5dad
Duonggsimp <152135459+Duonggsimp@users.noreply.github.com>
2024-02-09 18:18:50 +0700

    Create PHANTHUONG.cpp

---
+++ PHANTHUONG.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n;
+int main()
+{
+ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin>>n;
+    int x=(int)sqrt(n);
+    if (x%2==0)
+        x--;
+    for(int i=x;i>=1;i-=2)
+        if(n%i==0){
+            cout<<n/i;
+            return 0;
+        }
+    return 0;
+}
