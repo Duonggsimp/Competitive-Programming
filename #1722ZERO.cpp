a584edf49cfe9b64fa9abf2d35acfdc01c8dcb9f
Duonggsimp <152135459+Duonggsimp@users.noreply.github.com>
2024-02-18 13:40:39 +0700

    Update #1722ZERO.cpp

--- #1722ZERO.cpp
+++ #1722ZERO.cpp
@@ -1,25 +1,37 @@
-//http://chuyentin.ddns.net/problem.php?id=1722
-
 #include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define xn '\n'
 using namespace std;
-long n,ma,s;
+const int N = 1e3+5;
+const ll mo = 1e9+7;
 int main(){
-    cin>>n;
-    int a[n];
-    unordered_map<long,long>mp ;
-    for (long i;i<n;i++)
-        cin>>a[i];
-   for (long i;i<n;i++){
-    s+=a[i];
-    if (s==0)
-        ma=i+1;
-    if (mp.find(s)!=mp.end())
-        ma=max(ma,i-mp[s]);
-    else mp[s]=i;
-   }
-   if (ma==0)
-   cout<<-1; else
-    cout<<ma;
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    ll n,k;
+
+    cin >> n;
+    ll a[n+5];
+    a[0] = 0;
+    for (long i=1; i<=n; i++){
+        cin >> a[i];
+    }
+
+    map <ll, ll >mp;
+    ll s = 0;
+    ll maxx = -1;
+
+    for (ll i=1; i<=n; i++){
+        s+=a[i];
+        if (s==0)
+            maxx = max(maxx, i);
+        if (mp.find(s)!=mp.end())
+            maxx = max(maxx, i-mp[s]); else
+            mp[s] = i;
+    }
+    cout << maxx;
     return 0;
 }
-
