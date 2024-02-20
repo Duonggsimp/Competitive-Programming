d888d3c8fa2fa9c804104824809a5c193c24a318
Duonggsimp <152135459+Duonggsimp@users.noreply.github.com>
2024-02-20 19:40:10 +0700

    Update XGAME.cpp

--- XGAME.cpp
+++ XGAME.cpp
@@ -1,55 +1,45 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define xn '\n'
+#define se second
+#define pb push_back
+#define runfast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
-long long b[1000005]={},a[1000005];
-bool check[1000000];
-bool cmp(pair<long,long> x,pair<long,long> y){
-   if (x.first==y.first)
-    return x.second>y.second; else
-    return x.first<y.first;
-}
-bool SCP(long n){
-long tmp=sqrt(n);
-if (tmp*tmp==n)
-  return true; else
-  return false;
-}
-void sannt()
-{
-    memset(check,1,sizeof(check));
-    check[1]=false;
-    for (int i=2;i<=sqrt(1000000);i++)
-    if(check[i]==true)
-    for(int j=i;j<=1000000/i;j++)
-    check[i*j]=false;
+const int N=1e6+5;
+bool scp(ll n){
+    if (n==0)
+    return false; 
+  if (trunc(sqrt(n))*trunc(sqrt(n))==n)
+    return true; else
+    return false;
 }
 int main()
 {
-  long n,dem=0,x,c,m;
-  cin>>n;
-  for (long i=1;i<=n;i++){
-    cin>>a[i];
-  }
-  for (long i=1;i<=n;i++){
-      if (i<=n-10){
-    for (long j=i+1;j<=i+10;j++){
-      if (SCP(abs(a[j]-a[i]))&&(a[j]!=a[i])){
-          if (b[i]==0)
-          b[i]=1;
-        b[j]=max(b[j],b[i]+1);
-      }
-    }
-      }
-     else{
-        for (long j=i+1;j<=n;j++){
-      if (SCP(abs(a[j]-a[i]))&&(a[j]!=a[i])){
-          if (b[i]==0)
-          b[i]=1;
-        b[j]=max(b[i]+1,b[j]);
-      }
-    }
-   }
-  }
+  runfast;
 
-  cout<<*max_element(b+1,b+n+1);
+  #ifndef ONLINE_JUDGE
+  freopen(_FILE".inp","r",stdin);
+  freopen(_FILE".out","w",stdout);
+  #endif
+
+  ll n;
+   cin >> n;
+   ll a[n+5];
+   ll dp[n+5]={0};
+   for (long i=1; i<=n; i++){
+    cin >> a[i];
+   }
+
+   for (long i=1; i<=n; i++){
+       dp[i] = 1; 
+    long fi = max((long)1,i-10);
+    for (long j=fi; j<i; j++){
+        if (scp(abs(a[i]-a[j]))){
+        dp[i] = max(dp[i],dp[j]+1);
+         }
+      }
+   }
+  cout << *max_element(dp+1,dp+n+1) ;
   return 0;
 }
