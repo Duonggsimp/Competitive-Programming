fa0d8fb069f2b96d7523f48ff5cccdaa8b4c99a7
Duonggsimp <152135459+Duonggsimp@users.noreply.github.com>
2024-02-29 10:32:06 +0700

    Update COUNTINGROOM.cpp

--- COUNTINGROOM.cpp
+++ COUNTINGROOM.cpp
@@ -1,36 +1,47 @@
-#include<bits/stdc++.h>
-#define int long long
-#define endl "\n"
+#include <bits/stdc++.h>
+#define xn '\n'
+#define ll long long
+#define fi first
+#define se second
 using namespace std;
-int f[2001][2001]={};
-void fill(int i, int j)
-{
-    f[i][j]=2;
-    if (f[i+1][j]==1) fill(i+1,j);
-    if (f[i-1][j]==1) fill(i-1,j);
-    if (f[i][j+1]==1) fill(i,j+1);
-    if (f[i][j-1]==1) fill(i,j-1);
+const ll N=2e3+5;
+const ll mo=1e9+7;
+int f[N][N];
+char a;
+ll d;
+ll n,m;
+ll x,y;
+void dfs(ll x,ll y){
+    f[x][y]=1;
+    if (f[x-1][y]==0) dfs(x-1,y);
+    if (f[x+1][y]==0) dfs(x+1,y);
+    if (f[x][y-1]==0) dfs(x,y-1);
+    if (f[x][y+1]==0) dfs(x,y+1);
 }
-signed main()
-{
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int n,m,i,j,res=0; char a;
-    cin>>n>>m;
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=m;j++)
-        {
-            cin>>a;
-            if (a=='#') f[i][j]=2; else f[i][j]=1;
+
+int main(){
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+
+  cin >> n >> m;
+  memset(f,1,sizeof(f));
+  for (long i=1; i<=n; i++){
+        for (long j=1; j<=m; j++){
+            cin >> a;
+            if (a=='#')
+                f[i][j] = 1; else
+                f[i][j] = 0;
+        }
+  }
+  for (long i=1; i<=n; i++){
+    for (long j=1; j<=m; j++){
+        if (f[i][j]==0){
+            dfs(i,j);
+            d++;
         }
     }
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=m;j++)
-        {
-            if (f[i][j]==1) { res++; fill(i,j); }
-        }
-    }
-    cout<<res;
+  }
+  cout << d;
     return 0;
 }
