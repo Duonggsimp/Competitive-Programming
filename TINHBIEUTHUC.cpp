6f26c3b95a296d29394aa01294eaf35e3577f077
Duonggsimp <152135459+Duonggsimp@users.noreply.github.com>
2024-04-02 13:46:35 +0700

    Update TINHBIEUTHUC.cpp

--- TINHBIEUTHUC.cpp
+++ TINHBIEUTHUC.cpp
@@ -7,19 +7,16 @@ using namespace std;
 const int N = 1e5+5;
 const ll mo = 1e9+7;
 
-string s;
-string a[N];
 ll d;
 ll t1,t2;
+string s;
+string a[N];
 stack <ll> st;
 
 ll get(string x){
-if (x == "+")
-   return t1+t2; else
-if (x == "-")
-   return t2-t1; else
-if (x == "*")
-   return t1*t2;
+if (x == "+") return t1+t2; else
+if (x == "-") return t2-t1; else
+if (x == "*") return t1*t2;
 }
 
 int main(){
@@ -30,16 +27,12 @@ int main(){
     getline(cin,s);
     stringstream ss(s);
     string tmp;
-    while (ss >> tmp){
-        a[++d] = tmp;
-    }
-
+    while (ss >> tmp)  a[++d] = tmp;
+   
     for (long i=1; i<=d; i++){
         if (a[i] == "+" || a[i] == "-" || a[i] == "*"){
-            t1 = st.top();
-            st.pop();
-            t2 = st.top();
-            st.pop();
+            t1 = st.top(); st.pop();
+            t2 = st.top(); st.pop();
             st.push(get(a[i]));
         } else
         st.push(stoll(a[i]));
