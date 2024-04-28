16465830547d3a769c26e1c378b44df1aa831776
Phung Tan Dai Duong <152135459+Duonggsimp@users.noreply.github.com>
2024-04-28 20:54:35 +0700

    Update AVERAGE.cpp

--- AVERAGE.cpp
+++ AVERAGE.cpp
@@ -3,13 +3,14 @@
 #define fi first
 #define se second
 #define pb push_back
-#define runfast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
 using namespace std;
 const int N=1e5+5;
 long a[N], n, k, l, r, d ,vt ,ma;
-int main()
-{
-  runfast;
+int main(){
+  
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);cout.tie(0);
   cin >> n >> k;
   ll s = 0;
   for (long i=1; i<=n ;i++){
