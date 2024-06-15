153349db738948caf6d76e48720585d90577439f
Phung Tan Dai Duong <152135459+Duonggsimp@users.noreply.github.com>
2024-06-15 08:11:44 +0700

    Update COUNTINGROOM.cpp

--- COUNTINGROOM.cpp
+++ COUNTINGROOM.cpp
@@ -1,47 +1,51 @@
 #include <bits/stdc++.h>
-#define xn '\n'
 #define ll long long
 #define fi first
 #define se second
+#define xn '\n'
 using namespace std;
-const ll N=2e3+5;
-const ll mo=1e9+7;
-int f[N][N];
-char a;
-ll d;
+const int N = 1e3+5;
+const ll mo = 1e9+7;
+
 ll n,m;
-ll x,y;
-void dfs(ll x,ll y){
-    f[x][y]=1;
-    if (f[x-1][y]==0) dfs(x-1,y);
-    if (f[x+1][y]==0) dfs(x+1,y);
-    if (f[x][y-1]==0) dfs(x,y-1);
-    if (f[x][y+1]==0) dfs(x,y+1);
+ll res;
+char a[N][N];
+ll f[N][N];
+ll fx[4] = {0,0,-1,1};
+ll fy[4] = {-1,1,0,0};
+
+void dfs(ll x, ll y){
+    if (x >= 0 && x <= n && y >= 0 && y <= m && a[x][y] == '.'){
+        a[x][y] = '#';
+        for (long i=0; i<4; i++){
+            ll u = x + fx[i];
+            ll v = y + fy[i];
+            dfs(u,v);
+        }
+    }
 }
 
 int main(){
 
-  ios_base::sync_with_stdio(0);
-  cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
 
-  cin >> n >> m;
-  memset(f,1,sizeof(f));
-  for (long i=1; i<=n; i++){
+    cin >> n >> m;
+    for (long i=1; i<=n; i++){
         for (long j=1; j<=m; j++){
-            cin >> a;
-            if (a=='#')
-                f[i][j] = 1; else
-                f[i][j] = 0;
-        }
-  }
-  for (long i=1; i<=n; i++){
-    for (long j=1; j<=m; j++){
-        if (f[i][j]==0){
-            dfs(i,j);
-            d++;
+            cin >> a[i][j];
         }
     }
-  }
-  cout << d;
-    return 0;
+
+    for (long i=1; i<=n; i++){
+        for (long j=1; j<=m; j++){
+            if (a[i][j] == '.'){
+                res++;
+                dfs(i,j);
+            }
+        }
+    }
+
+    cout << res;
+   return 0;
 }
